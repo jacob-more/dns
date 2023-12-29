69f3f64049ff3c21617e30f57d9547a72de5f804
Jacob Moore <sagemore22@gmail.com>
2023-12-29 00:57:25 -0500

    Properly fill in blank RClass and TTL fields and abstracted `ResourceRecord` more.
    Just like how blank domain names are treated, blank TTL or RClass fields are filled in with whatever the value of the last defined one was.
    
    Created a new version of `ResourceRecord` for the `Tokenizer` that abstracts the `ResourceRecord` used by the `Entry`. This version does not allow for any of the required entries to be `Optional`. This will make parsing the resulting records much easier since they are always guaranteed to not be None.

--- dns-lib/src/serde/presentation/tokenizer/tokenizer.rs
+++ dns-lib/src/serde/presentation/tokenizer/tokenizer.rs
@@ -1,12 +1,36 @@
+use std::fmt::Display;
+
 use crate::serde::presentation::tokenizer::entry::Entry;
 
-use super::{entry::EntryIter, errors::TokenizerError};
+use super::{entry::{EntryIter, self}, errors::TokenizerError};
 
-pub use super::entry::ResourceRecord;
+#[derive(Clone, PartialEq, Eq, Hash, Debug)]
+pub struct ResourceRecord<'a> {
+    pub domain_name: &'a str,
+    pub ttl: &'a str,
+    pub rclass: &'a str,
+    pub rtype: &'a str,
+    pub rdata: Vec<&'a str>,
+}
+
+impl<'a> Display for ResourceRecord<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        writeln!(f, "Resource Record")?;
+        writeln!(f, "\tDomain Name: {}", self.domain_name)?;
+        writeln!(f, "\tTTL: {}", self.ttl)?;
+        writeln!(f, "\tClass: {}", self.rclass)?;
+        writeln!(f, "\tType: {}", self.rtype)?;
+        for rdata in &self.rdata {
+            writeln!(f, "\tRData: {}", rdata)?;
+        }
+        Ok(())
+    }
+}
 
 pub struct Tokenizer<'a> {
-    /// The most recent domain name. Used to fill in `None` domain names.
     last_domain_name: Option<&'a str>,
+    last_ttl: Option<&'a str>,
+    last_rclass: Option<&'a str>,
     /// The most recent domain name associated with a $ORIGIN token. Free standing `@` is replaced
     /// by the iterator. However, relative domain names are not updated by the iterator and may need
     /// to be updated by the user.
@@ -18,6 +42,8 @@ impl<'a> Tokenizer<'a> {
     pub fn new(feed: &'a str) -> Self {
         Tokenizer {
             last_domain_name: None,
+            last_ttl: None,
+            last_rclass: None,
             origin: None,
             entry_iter: EntryIter::new(feed),
         }
@@ -55,13 +81,43 @@ impl<'a> Iterator for Tokenizer<'a> {
                     // the last known domain name.
                     if let Some(this_domain_name) = rr.domain_name {
                         self.last_domain_name = Some(this_domain_name);
-                    } if let Some(last_domain_name) = self.last_domain_name {
+                    } else if let Some(last_domain_name) = self.last_domain_name {
                         rr.domain_name = Some(last_domain_name);
                     } else {
                         return Some(Err(TokenizerError::BlankDomainUsedBeforeDefined));
                     }
 
-                    return Some(Ok(rr));
+                    // Fill in any blank ttl's. If one is already defined, record it as being
+                    // the last known ttl.
+                    if let Some(this_ttl) = rr.ttl {
+                        self.last_ttl = Some(this_ttl);
+                    } else if let Some(last_ttl) = self.last_ttl {
+                        rr.ttl = Some(last_ttl);
+                    } else {
+                        return Some(Err(TokenizerError::BlankDomainUsedBeforeDefined));
+                    }
+
+                    // Fill in any blank classes. If one is already defined, record it as being
+                    // the last known class.
+                    if let Some(this_rclass) = rr.rclass {
+                        self.last_rclass = Some(this_rclass);
+                    } else if let Some(last_rclass) = self.last_rclass {
+                        rr.rclass = Some(last_rclass);
+                    } else {
+                        return Some(Err(TokenizerError::BlankDomainUsedBeforeDefined));
+                    }
+
+                    if let entry::ResourceRecord {
+                        domain_name: Some(domain_name),
+                        ttl: Some(ttl),
+                        rclass: Some(rclass),
+                        rtype,
+                        rdata 
+                    } = rr {
+                        return Some(Ok(ResourceRecord { domain_name, ttl, rclass, rtype, rdata }));
+                    } else {
+                        panic!("Unexpected Error: The resource record should have all required fields filled out but it does not.");
+                    }
                 }
             }
         }
