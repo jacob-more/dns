a94d558059feba011c3f204fee9f246a96522255
Jacob Moore <sagemore22@gmail.com>
2024-01-03 21:03:49 -0500

    Added a return type to the cache traits

--- dns-lib/src/interface/cache/transaction_cache.rs
+++ dns-lib/src/interface/cache/transaction_cache.rs
@@ -3,12 +3,12 @@ use async_trait::async_trait;
 use crate::query::message::Message;
 
 pub trait TransactionCache {
-    fn get(&self, query: &Message);
+    fn get(&self, query: &Message) -> Message;
     fn insert(&mut self, records: &Message);
 }
 
 #[async_trait]
 pub trait AsyncTransactionCache {
-    async fn get(&self, query: &Message);
+    async fn get(&self, query: &Message) -> Message;
     async fn insert(&mut self, records: &Message);
 }
