ae49700eb55b98c2187ae0b821499d5784d8a5be
Jacob Moore <sagemore22@gmail.com>
2024-12-23 21:39:16 -0500

    Removed the port-assignments file stored with the repository in favor of including it in a script so it can be downloaded as needed.

--- fetch-iana.sh
+++ fetch-iana.sh
@@ -1,5 +1,15 @@
 #!/bin/bash -e
 
+# Gets an updated version of the file "root.hints" from IANA. This file is used for boot-strapping
+# the DNS.
+
+wget -q -O "root.hints" "https://www.internic.net/domain/named.root"
+
+# Gets an updated version of the file "root.zone" from IANA. This file can be used for
+# boot-strapping the DNS.
+
+wget -q -O "root.zone" "https://www.internic.net/domain/root.zone"
+
 # Gets an updated version of the file "port-assignments.xml" from IANA. This file is used for
 # converting well-known-services into their respective port numbers. By using a configurable file,
 # custom ports can be added as needed.
