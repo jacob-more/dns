b2547f116de217622236e04b75f5bf36d85451e5
Jacob Moore <sagemore22@gmail.com>
2025-06-16 14:52:14 -0400

    Fixed use of `quote`'s built-in repetition for `ToPresentation`

--- dns-macros/src/to_presentation.rs
+++ dns-macros/src/to_presentation.rs
@@ -13,12 +13,13 @@ pub fn impl_to_presentation_macro(ast: &DeriveInput) -> proc_macro::TokenStream 
 fn impl_to_presentation_struct_macro(data: &DataStruct, ast: &DeriveInput) -> proc_macro::TokenStream {
     let name = &ast.ident;
     let field_name = data.fields.iter().map(|f| &f.ident);
+    let field_type = data.fields.iter().map(|f| &f.ty);
 
     quote! {
         impl crate::serde::presentation::to_presentation::ToPresentation for #name {
             #[inline]
             fn to_presentation_format(&self, out_buffer: &mut Vec<String>) {
-                #( crate::serde::presentation::to_presentation::ToPresentation::to_presentation_format(&self.#field_name, out_buffer); )*
+                #( <#field_type as crate::serde::presentation::to_presentation::ToPresentation>::to_presentation_format(&self.#field_name, out_buffer) );*
             }
         }
     }.into()
